b9cd1e0e952af3db111b80990ad38e7f1048b9ad
zinc <zinc97neo@proton.me>
2024-05-15 13:45:37 +0000

    refactor(wsl): separation of generic configurations

---
+++ system/base.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    helix # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
+    gnumake
+    wget
+    just # a command runner(replacement of gnumake in some cases)
+    curl
+  ];
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 2d";
+    };
+  };
+  system = {
+    autoUpgrade = {
+      enable = true;
+      channel = "https://nixos.org/channels/nixos-unstable";
+    };
+    stateVersion = "24.05";
+  };
+}
--- system/wsl.nix
+++ system/wsl.nix
@@ -1,33 +1,20 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
+    ./base.nix
   ];
-  wsl.enable = true;
-  wsl.defaultUser = "zinc";
-  environment.systemPackages = with pkgs; [
-    helix # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git
-    gnumake
-    wget
-    just # a command runner(replacement of gnumake in some cases)
-    curl
-  ];
+  wsl = {
+    enable = true;
+    wslConf.automount.root = "/mnt";
+    wslConf.interop.appendWindowsPath = false;
+    wslConf.network.generateHosts = false;
+    defaultUser = "zinc";
+    startMenuLaunchers = true;
+    # Enable integration with Docker Desktop (needs to be installed)
+    docker-desktop.enable = false;
+  };
   networking = {
     hostName = "wsl";
     networkmanager.enable = true;
-  };
-  nix = {
-    settings.experimental-features = [ "nix-command" "flakes" ];
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 2d";
-    };
-  };
-  system = {
-    autoUpgrade = {
-      enable = true;
-      channel = "https://nixos.org/channels/nixos-unstable";
-    };
-    stateVersion = "24.05";
   };
 }
