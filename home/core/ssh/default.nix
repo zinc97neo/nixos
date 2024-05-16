885756a20c11f33f096e9b0cd5a887ab8c724bfd
zinc <zinc97neo@proton.me>
2024-05-16 19:00:18 +0800

    feat(ssh): update config

--- home/core/ssh/default.nix
+++ home/core/ssh/default.nix
@@ -12,5 +12,11 @@
     enable = true;
     userName = "zinc";
     userEmail = "zinc97neo@proton.me";
+    extraConfig = {
+      url."git@github.com:".insteadOf = "https://github.com/";
+    };
+    lfs = {
+      enable = true;
+    };
   };
 }
