88404d9e7effaec99ba6e097cc47f311233b62b0
zinc <zinc97neo@proton.me>
2024-05-20 16:12:12 +0800

    feat(global): update config

--- home/gui/wm/wayland/niri/default.nix
+++ home/gui/wm/wayland/niri/default.nix
@@ -27,14 +27,4 @@
     package = pkgs.niri-unstable;
     config = null;
   };
-  home.packages = with pkgs; [
-    cage
-    qutebrowser
-    (wechat-uos.override {
-      uosLicense = pkgs.fetchurl {
-        url = "https://aur.archlinux.org/cgit/aur.git/plain/license.tar.gz?h=wechat-uos-bwrap";
-        hash = "sha256-U3YAecGltY8vo9Xv/h7TUjlZCyiIQdgSIp705VstvWk=";
-      };
-    })
-  ];
 }
--- outputs/linux.nix
+++ outputs/linux.nix
@@ -1,7 +1,9 @@
 { inputs, self, nixpkgs }:
 let
   system = "x86_64-linux";
-  pkgs = nixpkgs.legacyPackages.${system};
+  pkgs = import nixpkgs {
+    inherit system;
+  };
   configuration = inputs.home-manager.lib.homeManagerConfiguration;
 in
 {
