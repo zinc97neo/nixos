1d9c5e2c783598901e9bd3b6af20811e55cc3aee
zinc <zinc97neo@proton.me>
2024-06-01 17:25:17 +0800

    feat(app): install imv

--- home/gui/app/default.nix
+++ home/gui/app/default.nix
@@ -6,5 +6,18 @@
       defaultProfiles = [ "gpu-hq" ];
       scripts = [ pkgs.mpvScripts.mpris ];
     };
+    imv = {
+      enable = true;
+      settings = {
+        options = {
+          background = "24273a";
+          overlay_text_color = "cad3f5";
+          overlay_background_color = "181926";
+        };
+        aliases = {
+          q = "close";
+        };
+      };
+    };
   };
 }
